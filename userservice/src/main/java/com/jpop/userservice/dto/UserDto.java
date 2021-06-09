1c7a36c3ecb5672293fb6b9953ddb682709a065f
Sreenidhi Kunnavakkam <kvsreenidhi@gmail.com>
2021-06-09 15:26:16 +0530

    User service CRUD Operations

---
+++ userservice/src/main/java/com/jpop/userservice/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.jpop.userservice.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    Long id;
+
+    String name;
+
+    String address;
+}
---
+++ userservice/src/main/java/com/jpop/userservice/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.jpop.userservice.repository;
+
+import com.jpop.userservice.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends CrudRepository<User,Long> {
+
+}
