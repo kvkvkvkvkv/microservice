6ce5d6afc7e97a22474e5649162f9c2b7309845e
Sreenidhi Kunnavakkam <kvsreenidhi@gmail.com>
2021-06-09 12:51:21 +0530

    removing .git

---
+++ bookservice/src/main/java/com/jpop/bookservice/model/Book.java
@@ -0,0 +1,21 @@
+package com.jpop.bookservice.model;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Data
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    String name;
+
+    String author;
+}
